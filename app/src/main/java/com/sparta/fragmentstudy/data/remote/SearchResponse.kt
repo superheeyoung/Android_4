20dda72d003952ddf1e926f83016acabd4f0abd6
superheeyoung <tjdgmldud12@gmail.com>
2024-08-15 16:17:19 +0900

    Response 코드 정리

--- app/src/main/java/com/sparta/fragmentstudy/data/remote/SearchResponse.kt
+++ app/src/main/java/com/sparta/fragmentstudy/data/remote/SearchResponse.kt
@@ -33,3 +33,14 @@ data class VideoDocumentResponse(
     @SerializedName("author")
     val author: String,
 )
+
+data class ImageDocumentResponse(
+    @SerializedName("collection") val collection: String?,
+    @SerializedName("thumbnail_url") val thumbnailUrl: String?,
+    @SerializedName("image_url") val imageUrl: String?,
+    @SerializedName("width") val width: Int?,
+    @SerializedName("height") val height: Int?,
+    @SerializedName("display_sitename") val displaySitename: String?,
+    @SerializedName("doc_url") val docUrl: String?,
+    @SerializedName("datetime") val datetime: Date?
+)
