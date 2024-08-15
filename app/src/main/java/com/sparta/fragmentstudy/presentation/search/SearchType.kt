d57ba766f3f3e32b1e1e125c2036a08149880c52
superheeyoung <tjdgmldud12@gmail.com>
2024-08-15 12:55:42 +0900

    commit

--- app/src/main/java/com/sparta/fragmentstudy/presentation/search/SearchType.kt
+++ app/src/main/java/com/sparta/fragmentstudy/presentation/search/SearchType.kt
@@ -6,6 +6,6 @@ enum class SearchType(val viewType: Int) {
     DEFAULT(-1)
 }
 //서버에서 내려오는 값을 enum으로 변경할 때 사용
-fun setSearchType(viewType: Int): SearchType {
+fun findSearchType(viewType: Int): SearchType {
     return SearchType.entries.find { viewType == it.viewType } ?: SearchType.DEFAULT
 }
